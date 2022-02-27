a67300402cd43cb8ee0fd94867babf2754e9c6a4
ruchisharma03 <54274020+ruchisharma03@users.noreply.github.com>
2022-02-27 15:55:39 +0530

    Create create_issue.py

---
+++ scripts/create_issue.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import os
+import sys
+sys.path.append('.')
+
+from jira.jira import JiraAPI
+
+
+USERNAME = os.getenv('JIRA_USERNAME')
+API_TOKEN = os.getenv("JIRA_API_TOKEN")
+
+
+jira = JiraAPI(
+    USERNAME, API_TOKEN, "config/jira.config.yaml")
+
+print(jira.create_issue())
